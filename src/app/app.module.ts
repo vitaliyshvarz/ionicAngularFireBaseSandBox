8c53350cff55f4bff2b8d3635d0818d8dc07655d
vitaliy.chorniy <vitaliy.chorniy@mobisys.de>
2017-02-02 17:25:24 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbI2tXZ1OinqvMVCaX5jEpd_33jivacb8",
+  authDomain: "testapp-f17a3.firebaseapp.com",
+  databaseURL: "https://testapp-f17a3.firebaseio.com",
+  storageBucket: "testapp-f17a3.appspot.com",
+  messagingSenderId: "447020549120"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
