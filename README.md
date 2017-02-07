f7a0c486314b046c53e77b9b15dcc2a5c160f09a
Vitaliy Chorniy <chornij1@gmail.com>
2017-02-07 08:25:54 +0100

    added stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+you need NodeJS to be installed on your mashine
+
+ - $ npm i -g cordova ionic
+ - $ npm i
+ - $ ionic plugin add cordova-plugin-inappbrowser
---
+++ src/pages/login/login.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  songs: FirebaseListObservable<any>;
+
+  constructor() {
+  }
+
+
+}
